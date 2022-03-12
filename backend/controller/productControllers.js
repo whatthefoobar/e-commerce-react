8b164b20175d2043db0703248083610581257d95
whatthefoobar <irina.mntn@gmail.com>
2022-03-12 10:35:14 +0100

    built routes n controllers -backend done

---
+++ backend/controller/productControllers.js
@@ -0,0 +1,30 @@
+// here we do all the fetching
+// for that we first need  our model
+
+const Product = require("../models/Product");
+
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    res.json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+module.exports = {
+  getProducts,
+  getProductById,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 require("dotenv").config();
 const connectDB = require("./config/db");
+const productRoutes = require("./routes/productRoutes");
 
 connectDB();
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({ message: "API running..." });
+});
+
+//execute a middleware in the order that you give it on the program.
+app.use("/api/products", productRoutes);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
