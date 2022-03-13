04d6e17f6ff19ed5a3f1b7a3916e046a7329970a
whatthefoobar <irina.mntn@gmail.com>
2022-03-13 11:08:31 +0100

    navbar done for now

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,29 +1,27 @@
-import React from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      {/* logo */}
       <div className="navbar__logo">
-        <h2>Shopping cart</h2>
+        <h2>Shopping App</h2>
       </div>
-      {/* links */}
+
       <ul className="navbar__links">
         <li>
-          <Link to="/cart">
-            {/* Icon */}
+          <Link to="/cart" className="cart__link">
             <i className="fas fa-shopping-cart"></i>
-            Cart
-            <span className="cartlogo__badge">0</span>
+            <span>
+              Cart <span className="cartlogo__badge">0</span>
+            </span>
           </Link>
         </li>
         <li>
           <Link to="/">Shop</Link>
         </li>
       </ul>
-      {/* hamburger menu */}
+
       <div className="hamburger__menu">
         <div></div>
         <div></div>
