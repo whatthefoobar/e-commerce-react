1001fc72d4b1c34ff1b1e65ecffdbb467c0729b2
whatthefoobar <irina.mntn@gmail.com>
2022-03-17 16:05:38 +0100

    reducers done

---
+++ frontend/src/redux/actions/cartActions.js
@@ -0,0 +1,31 @@
+//make our http req to our backend to get the product details and add it to the cart
+import * as actionTypes from "../constants/cartConstants";
+import axios from "axios";
+
+//with redux thunk (dispatch and getState)
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/api/products/${id}`);
+
+  dispatch({
+    type: actionTypes.ADD_TO_CART,
+    payload: {
+      product: data._id,
+      name: data.name,
+      imageUrl: data.imageUrl,
+      price: data.price,
+      countInStock: data.countInStock,
+      qty,
+    },
+  });
+
+  localStorage.setItem("cart", JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  dispatch({
+    type: actionTypes.REMOVE_FROM_CART,
+    payload: id,
+  });
+
+  localStorage.setItem("cart", JSON.stringify(getState().cart.cartItems)); // check redux dev tools in browser under state
+};
