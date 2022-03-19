225bc04fbffc430ea39d44cec3102ac6665f53e5
whatthefoobar <irina.mntn@gmail.com>
2022-03-19 10:14:18 +0100

    Home screen w each Product is rendered after fetch

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -2,21 +2,19 @@ import React from "react";
 import "./Product.css";
 import { Link } from "react-router-dom";
 
-const Product = () => {
+const Product = ({ imageUrl, description, price, name, productId }) => {
   return (
     <div className="product">
-      <img
-        src="https://images.unsplash.com/photo-1507832321772-e86cc0452e9c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-        alt="product name"
-      />
+      <img src={imageUrl} alt={name} />
+
       <div className="product__info">
-        <p className="info__name">Product</p>
-        <p className="info__description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut vel
-          corporis eaque totam libero exercitationem repellat officiis quidem
-        </p>
-        <p className="info__price">$49.00</p>
-        <Link to={`/product/${1111}`} className="info__button">
+        <p className="info__name">{name}</p>
+
+        <p className="info__description">{description.substring(0, 100)}...</p>
+
+        <p className="info__price">${price}</p>
+
+        <Link to={`/product/${productId}`} className="info__button">
           View
         </Link>
       </div>
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,3 @@
-import React from "react";
 import "./HomeScreen.css";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -6,17 +5,39 @@ import { useDispatch, useSelector } from "react-redux";
 // Components
 import Product from "../components/Product";
 
+//Actions
+import { getProducts as listProducts } from "../redux/actions/productActions";
+
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
+  const getProducts = useSelector((state) => state.getProducts);
+  const { products, loading, error } = getProducts;
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
   return (
     <div className="homescreen">
       <h2 className="homescreen__title">Latest Products</h2>
       <div className="homescreen__products">
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        {loading ? (
+          <h2> Loading...</h2>
+        ) : error ? (
+          <h2>{error}</h2>
+        ) : (
+          products.map((product) => (
+            <Product
+              key={product._id}
+              name={product.name}
+              description={product.description}
+              price={product.price}
+              imageUrl={product.imageUrl}
+              productId={product._id}
+            />
+          ))
+        )}
       </div>
     </div>
   );
